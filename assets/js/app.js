8672d9ea2f818492daed86265522780c4619a770
MeepyMay <meepymay@gmail.com>
2018-06-26 17:23:52 -0400

    adding styling, still fixing the format for the form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,21 @@
-var topics = ["Wall-E", "The Incredibles", "Toy Story", "Spirited Away"];
+var topics = ["Rise of the Guardians", "Howl's Moving Castle", "Wreck-it Ralph", "The Incredibles", "My Neighbor Totoro", "Inside Out", "Big Hero 6", "Spirited Away"];
 var gifs = [];
-
-
 window.onload = function() {
 	$("#find-film").on("click", addFilm);
 	renderButtons();
 };
-
 $(document).on("click", "button", searchGifs);
 
 function searchGifs() {
 	var filmName = $(this).attr('data-name');
 	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + filmName + "&api_key=ipsNwKvIczYuK0mO76Im4fh4hMILbQlT&limit=10";
-    
-    renderGifs(queryURL);
+	renderGifs(queryURL);
 }
 
 function animateGif() {
 	var state = $(this).attr("data-state");
 	var animateState = $(this).attr("data-animate");
-    var stillState = $(this).attr("data-still");
-    
+	var stillState = $(this).attr("data-still");
 	if (state == "still") {
 		$(this).attr("src", animateState);
 		$(this).attr("data-state", "animate");
@@ -33,18 +28,16 @@ function animateGif() {
 
 function renderButtons() {
 	$("#buttons-view").empty();
-
 	for (var i = 0; i < topics.length; i++) {
 		var newBtn = $("<button>");
 		newBtn.attr("data-name", topics[i]);
 		newBtn.text(topics[i]);
 		$("#buttons-view").append(newBtn);
 	}
-  }
+}
 
-  function renderGifs(queryURL) {
+function renderGifs(queryURL) {
 	$("#films-view").empty();
-
 	$.ajax({
 		url: queryURL,
 		method: "GET"
@@ -52,30 +45,30 @@ function renderButtons() {
 		console.log(response);
 		var results = response.data;
 		for (var i = 0; i < results.length; i++) {
-			var animalDiv = $("<div>");
+			var filmDiv = $("<div>");
+			filmDiv.attr("class", "films");
 			var p = $("<p>");
-			p.text = "Rating: " + results[i].rating.toUpperCase();
-			var animalImage = $("<img>");
-			animalImage.attr("id", results[i].id);
-			animalImage.attr("src", results[i].images.fixed_height_still.url);
-			animalImage.attr("data-still", results[i].images.fixed_height_still.url);
-			animalImage.attr("data-animate", results[i].images.fixed_height.url);
-			animalImage.attr("data-state", "still");
-			animalImage.attr("class", "gif");
-			animalDiv.append(p.text);
-			animalDiv.append("<br>");
-			animalDiv.append(animalImage);
-			$("#films-view").prepend(animalDiv);
+			p.attr("class", "rating-text");
+			p.text("Rating: " + results[i].rating.toUpperCase());
+			var filmImage = $("<img>");
+			filmImage.attr("id", results[i].id);
+			filmImage.attr("src", results[i].images.fixed_height_still.url);
+			filmImage.attr("data-still", results[i].images.fixed_height_still.url);
+			filmImage.attr("data-animate", results[i].images.fixed_height.url);
+			filmImage.attr("data-state", "still");
+			filmImage.attr("class", "gif");
+			filmDiv.append(p);
+			filmDiv.append(filmImage);
+			$("#films-view").prepend(filmDiv);
 			gifs.push(results[i]);
 		}
 		$(".gif").on("click", animateGif);
 	});
-  }
+}
 
-function addFilm (event) {
-	event.preventDefault();	
+function addFilm(event) {
+	event.preventDefault();
 	var film = $("#film-input").val();
 	topics.push(film);
-
 	renderButtons();
-  }
+}

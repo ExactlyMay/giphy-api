5c8daa763d74b88b1b75820e467d8711b4fafed7
MeepyMay <meepymay@gmail.com>
2018-06-26 22:31:43 -0400

    finished a few bonuses

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,25 @@
 var topics = ["Rise of the Guardians", "Howl's Moving Castle", "Wreck-it Ralph", "The Incredibles", "My Neighbor Totoro", "Inside Out", "Big Hero 6", "Spirited Away", "Toy Story", "Megamind"];
 var gifs = [];
+var filmName = "";
+
 window.onload = function() {
 	$("#find-film").on("click", addFilm);
 	renderButtons();
 };
-$(document).on("click", "button", searchGifs);
+
+$(document).ready(function(){
+	$('#films-view').on('click', '.gif', animateGif);
+	$(document).on("click", ".topics", searchGifs);
+	$(document).on("click", "#add-gifs", addGifs);
+});
+
 
 function searchGifs() {
-	var filmName = $(this).attr('data-name');
-	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + filmName + "&api_key=ipsNwKvIczYuK0mO76Im4fh4hMILbQlT&limit=10";
-	renderGifs(queryURL);
+	if(filmName != $(this).attr('data-name')){
+		$("#films-view").empty();
+	}
+	filmName = $(this).attr('data-name');
+	addGifs();
 }
 
 function animateGif() {
@@ -31,13 +41,20 @@ function renderButtons() {
 	for (var i = 0; i < topics.length; i++) {
 		var newBtn = $("<button>");
 		newBtn.attr("data-name", topics[i]);
+		newBtn.attr("class", "topics");
 		newBtn.text(topics[i]);
 		$("#buttons-view").append(newBtn);
 	}
 }
 
+function addGifs() {
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + filmName + "&api_key=ipsNwKvIczYuK0mO76Im4fh4hMILbQlT&limit=10&offset=" + gifs.length;
+	renderGifs(queryURL);
+}
+
 function renderGifs(queryURL) {
-	$("#films-view").empty();
+	
+	$("#add-gifs").remove();
 	$.ajax({
 		url: queryURL,
 		method: "GET"
@@ -46,11 +63,14 @@ function renderGifs(queryURL) {
 		var results = response.data;
 		for (var i = 0; i < results.length; i++) {
 			var filmDiv = $("<div>");
-			filmDiv.attr("class", "films");
 			var p = $("<p>");
-			p.attr("class", "rating-text");
-			p.text("Rating: " + results[i].rating.toUpperCase());
 			var filmImage = $("<img>");
+
+			filmDiv.attr("class", "films");
+			
+			p.attr("class", "rating-text");
+			p.append( "Title: " + results[i].title.toUpperCase() + "<br>" + "Rating: " + results[i].rating.toUpperCase());
+		
 			filmImage.attr("id", results[i].id);
 			filmImage.attr("src", results[i].images.fixed_height_still.url);
 			filmImage.attr("data-still", results[i].images.fixed_height_still.url);
@@ -59,10 +79,11 @@ function renderGifs(queryURL) {
 			filmImage.attr("class", "gif");
 			filmDiv.append(p);
 			filmDiv.append(filmImage);
-			$("#films-view").prepend(filmDiv);
+			$("#films-view").append(filmDiv);
 			gifs.push(results[i]);
 		}
-		$(".gif").on("click", animateGif);
+		// $(".gif").on("click", animateGif);
+		$("#films-view").append("<button id='add-gifs'> Add 10 more... </button>");
 	});
 }
 
